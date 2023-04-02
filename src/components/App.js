eb67868d3d0c07530399b0f7d4317090635e8f5e
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-04-02 19:18:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [value,setValue] = useState(0);
+  const [sum,setSum] = useState(0);
+  
+const handleChange = (e)=>{
+       setValue(parseInt(e.target.value));
+          const read = sum+parseInt(e.target.value);
+          setSum(read);
+           
+}
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+       
+        <input className="input"  type="number"  value={value} min='-100' max='100' onChange={handleChange}
+     />
+
+           
+            <div>
+              <p>Sum: {sum}</p>
+            </div>
+        
     </div>
   )
 }
+
 
 export default App
